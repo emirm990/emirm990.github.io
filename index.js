6c7647fcca6ea3df2cf204e06be4db553c95899a
emirm990 <emir.mekic@live.com>
2018-11-13 11:50:03 +0100

    some minor changes

---
+++ index.js
@@ -0,0 +1,22 @@
+let navButton = document.getElementById("nav-expand");
+let navigation = document.getElementById("navigation");
+let navigationList = document.getElementById("nav-ul");
+let progressBar = document.getElementById("progressBar");
+navButton.addEventListener("click", function(){
+    navigationList.classList.toggle("block");
+    navigation.classList.toggle("block");
+    if (navigation.classList.contains("block")){
+        progressBar.style.top = "0";
+    }else progressBar.style.top = "25px";
+    
+})
+
+function scrollbar(){
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    progressBar.style.width = scrolled + "%";
+}
+window.onscroll = function(){
+    scrollbar();
+}

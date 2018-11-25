a0ffe8f22817f6d651db8ddc5146f2a6685f53a5
emirm990 <emir.mekic@live.com>
2018-11-25 18:49:17 +0100

    style and functionality

--- vjezbe/to-to-list/index.js
+++ vjezbe/to-to-list/index.js
@@ -3,12 +3,17 @@ let list = document.getElementById("list");
 let remove = document.getElementsByClassName("remove");
 let key = document.getElementsByClassName("key");
 let login = document.getElementById("login");
+let logout = document.getElementById("logout");
 let addItem = document.getElementById("confirm_item");
+let loginContainer = document.getElementById("login-container");
+let logoutContainer = document.getElementById("logout-container");
+let usernameContainer = document.getElementById("username-container");
+let checkedOrNot;
+let color = "black";
 
 login.addEventListener("click", function(){
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -16,6 +21,13 @@ login.addEventListener("click", function(){
         alert("Error: " + errorMessage);
         // ...
     });
+})
+logout.addEventListener("click", function(){
+    firebase.auth().signOut().then(function() {
+          // Sign-out successful.
+        }).catch(function(error) {
+            alert("Error: " +error);
+        });
 })
 
 firebase.auth().onAuthStateChanged(function(user) {
@@ -29,19 +41,32 @@ firebase.auth().onAuthStateChanged(function(user) {
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
         var providerData = user.providerData;
+        logoutContainer.style.display = "block";
+        loginContainer.style.display = "none";
+        usernameContainer.innerText = user.email;
+        addItem.disabled = false;
         fetch("https://to-do-5b78c.firebaseio.com/list.json")
             .then(response => {return response.json()})
             .then(data => {
                 for(const key in data){
-                    list.innerHTML += `<li>
-                    <span>${data[key].username}</span>
-                    <span>${data[key].item}</span>
-                    <span>${data[key].date}</span>
+                    if (data[key].checked==true){
+                        checkedOrNot = "checked";
+                        color = "green";
+                    }else {
+                        checkedOrNot = " ";
+                        color = "black";
+                    }
+
+                    list.innerHTML += `<li style = "color:${color}"></>
+                    <span class="email-field">${data[key].username}</span>
+                    <span class="text-field">${data[key].item}</span>
+                    <span class="date-field">${data[key].date}</span>
                     <span class="key">${key}</span>
-                    <button class="remove">-</button>
+                    <input type="checkbox" class="remove" ${checkedOrNot} input>
                     </li>`
                 }
             })
+        checkedColor();
         addItem.addEventListener("click", postToDoItem);
         function postToDoItem(){
             let username = user.email;
@@ -56,34 +81,57 @@ firebase.auth().onAuthStateChanged(function(user) {
                 item: item,
                 date: date,
                 checked: checked
+                })
             })
-        })
-        list.innerHTML += `<li>
-                    <span>${username}</span>
-                    <span>${item}</span>
-                    <span>${date}</span>
+            list.innerHTML += `<li>
+                    <span class="email-field">${username}</span>
+                    <span class="text-field">${item}</span>
+                    <span class="date-field">${date}</span>
                     <span class="key">${key}</span>
-                    <button class="remove">-</button>
+                    <input type="checkbox" class="remove" ${checkedOrNot}></input>
                     </li>`;
+            checkedColor();
+            
         }
         
         list.addEventListener("click", (event) => {
+            console.log(event.target);
             if(event.target.classList.contains("remove")){
-                event.target.parentNode.remove();
-                fetch("https://to-do-5b78c.firebaseio.com/list/" 
-                + event.target.previousElementSibling.innerText 
-                + ".json",{
-                    method: "delete"
-                })
+                let target = event.target.previousElementSibling;
+                console.log(target);
+                function writeUserData(target) {
+                    if (event.target.checked){
+                        firebase.database().ref('list/' + target.innerText).update({
+                        checked: true
+                    });
+                    event.target.parentElement.style.color = "green";
+                    }else {
+                        firebase.database().ref('list/' + target.innerText).update({
+                            checked: false
+                        });
+                        event.target.parentElement.style.color = "black";
+                    }
+                }
+                writeUserData(target);
             }
         })
           // ...
     } else {
       // User is signed out.
       // ...
-      alert("Logged Out");
+        logoutContainer.style.display = "none";
+        loginContainer.style.display = "block";
+        list.innerText = "";
+        addItem.disabled = true;
     }
   });
-
+function checkedColor(){
+    let checkedBox = document.getElementsByClassName("remove");
+    console.log(checkedBox.length);
+    if (checkedBox.checked){
+        checkedBox.parentElement.stye.color = "green";
+        
+    }
+}
 
 

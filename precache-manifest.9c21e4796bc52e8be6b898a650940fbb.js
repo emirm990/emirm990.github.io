8fccca5b1ce86ab56e120241b84e5be46df59d8d
emirm990 <emir.mekic@live.com>
2019-01-21 17:15:51 +0100

    Updates

---
+++ precache-manifest.9c21e4796bc52e8be6b898a650940fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6534f2da5c69e226f573",
+    "url": "/vjezbe/caffe-pos/static/js/runtime~main.6534f2da.js"
+  },
+  {
+    "revision": "fbddad321a5e709a33e3",
+    "url": "/vjezbe/caffe-pos/static/js/main.fbddad32.chunk.js"
+  },
+  {
+    "revision": "e135f13b5c7be32b41b4",
+    "url": "/vjezbe/caffe-pos/static/js/1.e135f13b.chunk.js"
+  },
+  {
+    "revision": "e135f13b5c7be32b41b4",
+    "url": "/vjezbe/caffe-pos/static/css/1.1d368bc1.chunk.css"
+  },
+  {
+    "revision": "a01154169da60c388f9e45932de40038",
+    "url": "/vjezbe/caffe-pos/index.html"
+  }
+];

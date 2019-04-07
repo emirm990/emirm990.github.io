c213ddebf66d599b6dbd90b16ab6e398c39147ff
Emir Mekić <emir.mekic@live.com>
2019-04-07 15:01:22 +0200

    visual changes

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-let navButton = document.getElementById("nav-expand");
-let navigation = document.getElementById("navigation");
-let navigationList = document.getElementById("nav-ul");
-let progressBar = document.getElementById("progressBar");
-let menuIcon = document.getElementById("menu-icon");
-
-/*navButton.addEventListener("click", function(){
-    navigationList.classList.toggle("block");
-    navigation.classList.toggle("block");
-
-    if (navigation.classList.contains("block")){
-        
-        progressBar.style.top = "0";
-    }else {
-        progressBar.style.top = "25px";
-        
-    }
-    //todo menuIcon.style.transform = "rotate(90deg)";
-})*/
-
 function scrollbar() {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   let height =
